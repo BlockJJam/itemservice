accfc61d8cee42bca917ccc4a426fe45aabc096c
BlockJJam <blockjjam99@gmail.com>
2021-08-15 17:49:41 +0900

    update: After first commit, Last commit
    간단한 상품 서비스를 통해 타임리프와 스프링 MVC구조를 학습하면서, 편리성과 그 의미를 알아볼 수 있었습니다. 학습한 코드를 올리지만 직접 쳐보시길..

--- src/main/java/com/example/itemservice/web/basic/BasicItemController.java
+++ src/main/java/com/example/itemservice/web/basic/BasicItemController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -36,9 +37,77 @@ public class BasicItemController {
         return "basic/addForm";
     }
 
-    @PostMapping("/add")
-    public String save(){
-        return ";;";
+    //@PostMapping("/add")
+    public String addItemV1(@RequestParam String itemName,
+                       @RequestParam int price,
+                       @RequestParam int quantity,
+                       Model model){
+        Item item = new Item();
+        item.setItemName(itemName);
+        item.setPrice(price);
+        item.setQuantity(quantity);
+
+        itemRepository.save(item);
+
+        model.addAttribute("item", item);
+        return "basic/item";
+    }
+
+    //@PostMapping("/add")
+    public String addItemV2(@ModelAttribute("item") Item item){
+        // Item 객체에 그대로 담아 오기 때문에 Item 새로 만들 필요 없음
+        // ModeAttribute의 (name="") 옵션 : model.addAttribute에 자동으로 들어감
+
+        itemRepository.save(item);
+
+        //model.addAttribute("item", item);
+        return "basic/item";
+    }
+
+   // @PostMapping("/add")
+    public String addItemV3(@ModelAttribute Item item){
+        // Item 객체에 그대로 담아 오기 때문에 Item 새로 만들 필요 없음
+        // ModeAttribute의 (name="") 옵션 : model.addAttribute에 자동으로 들어감
+        // Model Attribute에 담기는 원리(name지정 안할 시)
+        //      : HelloData -> helloData
+        //      : User -> user
+        itemRepository.save(item);
+
+        //model.addAttribute("item", item);
+        return "basic/item";
+    }
+
+    //@PostMapping("/add")
+    public String addItemV4( Item item){
+        itemRepository.save(item);
+
+        return "redirect:/basic/items/"+item.getId();
+    }
+
+        @PostMapping("/add")
+        public String addItemV5(Item item, RedirectAttributes redirectAttributes){
+            // redirect할 때 parameter를 붙여서 보내서 확인 메시지를 띄운다
+            Item savedItem = itemRepository.save(item);
+            redirectAttributes.addAttribute("itemId", savedItem.getId());
+            redirectAttributes.addAttribute("status", true);
+
+            return "redirect:/basic/items/{itemId}";
+            // query parameter가 포함되서 나간다 -> ?status=true;
+        }
+
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable long itemId, Model model){
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+
+        return "basic/editForm";
+    }
+
+    @PostMapping("/{itemId}/edit")
+    public String edit(@PathVariable long itemId, @ModelAttribute Item item){
+        itemRepository.update(itemId, item);
+
+        return "redirect:/basic/items/{itemId}";
     }
 
     //미리 메모리에 test용 item 추가

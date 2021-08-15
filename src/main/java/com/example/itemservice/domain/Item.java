26d24d9b4bd7c09a4953e04f84ee0b728de46c62
BlockJJam <blockjjam99@gmail.com>
2021-08-15 14:41:46 +0900

    feat: First Commit
    스프링 MVC 구조와 Thymeleaf를 활용해 간단한 item service 애플리케이션을 만들어보자

---
+++ src/main/java/com/example/itemservice/domain/Item.java
@@ -0,0 +1,23 @@
+package com.example.itemservice.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class Item {
+
+    private Long id;
+    private String itemName;
+    private Integer price;
+    private Integer quantity;
+
+    public Item(String itemName, Integer price, Integer quantity) {
+        this.itemName = itemName;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
---
+++ src/main/java/com/example/itemservice/web/basic/BasicItemController.java
@@ -0,0 +1,50 @@
+package com.example.itemservice.web.basic;
+
+import com.example.itemservice.domain.Item;
+import com.example.itemservice.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+@Controller
+@RequestMapping("/basic/items")
+@RequiredArgsConstructor
+public class BasicItemController {
+
+    private final ItemRepository itemRepository;
+
+    @GetMapping()
+    public String items(Model model){
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "basic/items";
+    }
+
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable long itemId, Model model){
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/item";
+    }
+
+    @GetMapping("/add")
+    public String addForm(){
+        return "basic/addForm";
+    }
+
+    @PostMapping("/add")
+    public String save(){
+        return ";;";
+    }
+
+    //미리 메모리에 test용 item 추가
+    @PostConstruct
+    public void init(){
+        itemRepository.save(new Item("itemA", 10000, 10));
+        itemRepository.save(new Item("itemB", 20000, 20));
+    }
+}
